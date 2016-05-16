6d82940faced6cdb775602e669d508e93c2bac5e
Shane Steward <shane-steward@hotmail.com>
2016-05-16 19:15:32 -0400

    Updates

--- client/main.js
+++ client/main.js
@@ -5,22 +5,13 @@ import './main.html';
 
 Template.blog.events({
   // counter starts at 0
-  'submit #blogForm'function(e){
+  'submit #blogForm':function(e){
   e.preventDefault();
-  var title= $('#blogTitle').val();
+  var title=$('#blogTitle').val();
   var body=$('#blogSubject').val();
   Meteor.call('submitPost',title,body);
 });
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
-
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
-});
+Template.listBlogs.blogs = function(){
+return Blogs.find();
+}
--- server/main.js
+++ server/main.js
@@ -1,5 +1,12 @@
 import { Meteor } from 'meteor/meteor';
-
 Meteor.startup(() => {
   // code to run on server at startup
 });
+
+Meteor.methods({
+'submitPost':function(title,body){
+console.log(title);
+console.log(body);
+
+}
+})
